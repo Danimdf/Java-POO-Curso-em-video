b4942bba6b30ad3f83f44fac5e94eba47ee2c3fd
Daniela Monteiro <danimdf360@gmail.com>
2022-02-07 20:18:39 -0300

    Visibility in Curso em Video

---
+++ Aula02/src/com/cursoEmVideo/POOJava/Aula3/Aula3.java
@@ -0,0 +1,8 @@
+package com.cursoEmVideo.POOJava.Aula3;
+import com.cursoEmVideo.POOJava.Aula3.Caneta;
+public class Aula3 {
+    public static void main (String[] args) {
+        Caneta c1 = new Caneta();
+
+    }
+}
