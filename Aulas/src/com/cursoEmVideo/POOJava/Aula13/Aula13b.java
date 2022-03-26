2e593f4f5583c815093ed710ff6bfa4ef423a6a9
Daniela Monteiro <danimdf360@gmail.com>
2022-03-26 20:52:10 -0300

    Feat: Studying method overload in polymorphism POO

---
+++ Aulas/src/com/cursoEmVideo/POOJava/Aula13/Aula13b.java
@@ -0,0 +1,13 @@
+package com.cursoEmVideo.POOJava.Aula13;
+
+public class Aula13b {
+    public static void main(String[] args) {
+        Cachorro c = new Cachorro();
+
+        c.reagir("Olá");
+        c.reagir("Auuuuuuu");
+        c.reagir(true);
+        c.reagir(22, 33);
+        c.reagir(1, 12.5f);
+    }
+}
---
+++ Aulas/src/com/cursoEmVideo/POOJava/Aula13/Lobo.java
@@ -0,0 +1,9 @@
+package com.cursoEmVideo.POOJava.Aula13;
+
+public class Lobo extends Mamifero{
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Auuuuuuuuuuuuu!");
+    }
+}
